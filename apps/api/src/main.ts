8ebabc9e4f8d2de63117001cfd09af7d8bcb4c1d
ZeBolHD <zezebolz@gmail.com>
2024-05-21 07:03:57 +0300

    chore(api): added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle("Schedule bot API")
+    .setDescription("The Schedule bot API description")
+    .setVersion("1.0")
+    .addTag("Schedule bot API")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger", app, document);
 
   await app.listen(3500);
 }
